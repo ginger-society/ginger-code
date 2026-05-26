cb8232e9642c5561fceb3886cff787943dd3b9c0
Ginger Society Admin <admin@gingersociety.org>
2026-05-26 11:53:30 +0530

    chore: building

--- bundle.sh
+++ bundle.sh
@@ -8,6 +8,7 @@ rm -rf $APP
 mkdir -p $APP/Contents/MacOS
 
 cp target/release/ginger-code $APP/Contents/MacOS/ginger-code
+cp target/release/ginger-code-cli $APP/Contents/MacOS/ginger-code-cli
 cp Info.plist $APP/Contents/
 
 echo "Built $APP"
